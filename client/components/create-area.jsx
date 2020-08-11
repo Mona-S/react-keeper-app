c7364c9d02b35145d1ffea295cf062d0af7fbc03
Mona-S <forward2cm@gmail.com>
2020-08-11 16:30:19 +0000

    created form tag and add button

--- client/components/create-area.jsx
+++ client/components/create-area.jsx
@@ -3,8 +3,11 @@ import React from "react";
 function CreateArea(){
     return(
         <div>
-            <form action="">
-                
+            <form>
+                <input placeholder="Title"/>
+                <textarea name="" placeholder="Write a note" rows="10"></textarea>
+                <button>Add</button>
+
             </form>
         </div>
     )
