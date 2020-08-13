bfb50afec584c5280226f899802ca268fc50a2ea
Mona-S <forward2cm@gmail.com>
2020-08-13 13:31:04 +0000

    added the useState and initialised its value as an object

--- client/components/create-area.jsx
+++ client/components/create-area.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea(){
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+
+    });
+
     return(
         <div>
             <form>
                 <input placeholder="Title"/>
-                <textarea name="" placeholder="Write a note" rows="10"></textarea>
+                <textarea name="" placeholder="Write a note" rows="3"></textarea>
                 <button>Add</button>
 
             </form>
